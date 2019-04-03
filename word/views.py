2ae7af31f2c9e6aaab95b0922add182c29216bb7
Yoona-Choi <yoon8696@naver.com>
2019-04-03 21:14:33 +0900

    first commit

---
+++ word/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def result(request):
+    text = request.GET['fulltext']
+    words = text.split ()
+    word_count = {}
+    for word in words:
+        if word in word_count:
+            word_count[word]+=1
+        else:
+            word_count[word]=1
+    return render(request, 'result.html', {'full' : text, 'total' :len(words),'count' : word_count.items()})
+
